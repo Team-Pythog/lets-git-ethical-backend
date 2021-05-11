35dd4b86afc12179c778d92040b24d3adbaf288b
Alex Angelico <alex.angelico@live.com>
2021-05-11 03:37:47 -0700

    changed ethics model field names; added JWT refresh auth; added token refresh route

--- ethics_app/models.py
+++ ethics_app/models.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 class Dilemmas(models.Model):
   title = models.CharField(max_length=64)
   image = models.ImageField(upload_to='images', blank=True)
-  dilemma = models.TextField()
-  optionA = models.CharField(max_length=64, blank=True, default='')
-  optionB = models.CharField(max_length=64, blank=True, default='')
+  text = models.TextField()
+  response_0 = models.CharField(max_length=64, blank=True, default='')
+  response_1 = models.CharField(max_length=64, blank=True, default='')
 
   def __str__(self):
     return self.title
 
 class ProfileInfo(models.Model):
-  thumbnail = models.ImageField(upload_to='ethics_app/image_assets', blank=True)
+  image = models.ImageField(upload_to='ethics_app/image_assets', blank=True)
   header = models.CharField(max_length=64)
-  header_image = models.ImageField(upload_to='ethics_app/image_assets', blank=True)
+  banner = models.ImageField(upload_to='ethics_app/image_assets', blank=True)
   bio = models.TextField()
   gender = models.CharField(max_length=32)
   instagram = models.URLField(max_length=200, blank=True)
--- lets_git_ethical/urls.py
+++ lets_git_ethical/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
+    path('token-auth/refresh', refresh_jwt_token),
     path('account/', include('account.urls')),
     path('ethics_app/', include('ethics_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
